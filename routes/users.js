3ea8d5f7667bb2eab328c5fed763152417a20323
Chris Sharkey <ev2900@gmail.com>
2017-01-08 08:49:52 -0500

    working on signup route

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,16 @@ router.get('/login', function(req, res) {
 	res.render('login');
 });
 
+// get register
+router.post('/register', function(req, res) {
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	console.log(name);
+});
+
+
 module.exports = router;
