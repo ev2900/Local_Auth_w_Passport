ef3d690aba57f40af88d34b1a63b7cf29fa8401a
Chris Sharkey <ev2900@gmail.com>
2017-01-08 22:28:49 -0500

    ensureAuthenticated

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 // get index
-router.get('/', function(req, res) {
+router.get('/', ensureAuthenticated, function(req, res) {
 	res.render('index');
 });
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthticated()){
+		return next();
+	} else {
+		req.flash('error_msg', 'You are not logged in');
+		res.redirect('users/login');
+	}
+}
 
 module.exports = router;
