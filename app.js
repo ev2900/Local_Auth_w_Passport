dca0c0c160cebe7e357a9250e09a8e92eaf0dc4f
Chris Sharkey <ev2900@gmail.com>
2017-01-08 22:21:43 -0500

    layout will change depending on login/out satus

--- app.js
+++ app.js
@@ -70,8 +70,11 @@ app.use(function (req, res, next) {
 	res.locals.success_msg = req.flash('success_msg');
 	res.locals.error_msg = req.flash('eror_msg');
 	res.locals.error = req.flash('error');
+	res/locals.user = req.user || null;
 	next(); 
 });
+
+
 
 // Route files
 app.use('/',  routes);
