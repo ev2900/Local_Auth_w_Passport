2d160007a5fde1dc8b66cf36410f92a7f4d19557
Chris Sharkey <ev2900@gmail.com>
2017-01-08 22:14:33 -0500

    misspelling

--- models/user.js
+++ models/user.js
@@ -38,7 +38,7 @@ module.exports.getUserById = function(id, callback) {
 	User.findById(id, callback);
 }
 
-modele.exports.comparePassword = function(candidatePassword, hash, callback) {
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
 	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
 		if(err) throw err;
 		callback(null, isMatch);
