a1c396bc3ef1e1ad33b4b4b84e305695f53e5d55
Chris Sharkey <ev2900@gmail.com>
2017-01-08 22:18:08 -0500

    added logout

--- routes/users.js
+++ routes/users.js
@@ -92,4 +92,12 @@ router.post('/login',
 		res.redirect('/');		
 	});
 
+router.get('/logout', function(req, res){
+	req.logout();
+
+	req.flash('success_msg', 'You are logged out');
+
+	res.redirect('/users/login');
+});
+
 module.exports = router;
